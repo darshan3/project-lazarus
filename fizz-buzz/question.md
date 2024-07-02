3a02f6f12e63f24b297657e20058a9603b8e76af
Darshan Tank <darshantank3@gmail.com>
2024-07-02 22:34:12 +0530

    fizz-buzz 24/07/02

---
+++ fizz-buzz/question.md
@@ -0,0 +1,2 @@
+Leetcode: https://leetcode.com/problems/fizz-buzz/
+InterviewBit: https://www.interviewbit.com/problems/fizzbuzz/
---
+++ fizz-buzz/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> result;
+
+        for(int i=1; i<=n; i++) {
+            result.push_back(
+                ( i % 15 == 0 ) ? "FizzBuzz" :
+                ( i % 5 == 0 ) ? "Buzz" :
+                ( i % 3 == 0 ) ? "Fizz" :
+                to_string(i)
+            );
+        }
+
+        return result;
+    }
+};
